fc5619dddad38deb7fd554605899d18bd6d766a8
WinstonAdams <apollo07291@gmail.com>
2023-12-19 11:35:35 +0800

    feat: add error handler

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const exchangeRateController = require('../controllers/exchange-rate-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.get('/', exchangeRateController.exchangeRate)
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
